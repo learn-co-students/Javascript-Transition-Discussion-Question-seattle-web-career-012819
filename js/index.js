f0142507db1812ab07c5bbc961e5a49711480df9
jasonbasuil <basuilj@gmail.com>
2019-03-13 09:50:50 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,33 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// Using jQuery
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
+const button = document.getElementById('save_lincoln')
+const foreground = document.getElementById('foreground')
+const hiddenMessage = document.getElementById('hidden-message')
+
+
+button.addEventListener('click', function(){
+  fade(foreground)
+  hiddenMessage.style.display = 'inline'
+});
+
+function fade(element) {
+  const interval = document.getElementById('interval').value
+  var op = 1;  // initial opacity
+  var timer = setInterval(function () {
+      if (op <= 0.1){
+          clearInterval(timer);
+          element.style.display = 'none';
+      }
+      element.style.opacity = op;
+      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+      op -= op * 0.1;
+  }, interval);
 }
