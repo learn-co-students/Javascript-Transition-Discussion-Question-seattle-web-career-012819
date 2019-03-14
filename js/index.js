925c1c67b12b54cde00f744f8d7614b879ae031c
omfgtora <spaminol@gmail.com>
2019-03-13 17:37:06 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+
+
+const button = document.getElementById('save_lincoln')
+const foreground = document.getElementById('foreground')
+
+button.addEventListener('click', function(){
+  let input = document.getElementById('interval').value
+  saveLincoln(input)
+})
 
 function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
+  if(isNaN(interval) || interval == "") {
+    interval = 1000
   }
-  $('#foreground').fadeOut(interval);
+  // $('#foreground').fadeOut(interval);
+  foreground.style.transition = `opacity ${interval}ms, transform ${interval}ms`
+  foreground.style.opacity = "0"
+  foreground.style.transform = "scale(3) "
+  document.getElementById('hidden-message').style.display = ""
 }
